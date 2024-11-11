723d160af2c7b49a2d3ec572a94704c321a20028
Dragos Ungureanu <ungureanud6@gmail.com>
2024-11-11 21:54:49 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "jsm-fh",
-    project: "javascript-react"
-  })],
+  base: "/iPhone15-Page",
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "jsm-fh",
+      project: "javascript-react",
+    }),
+  ],
 
   build: {
-    sourcemap: true
-  }
-})
+    outDir: "dist",
+    sourcemap: true,
+  },
+});
